2a74b590be2d43867f67d2655c30e5eb3bc19a4b
tperera27 <peretaty27@gmail.com>
2021-11-11 16:42:57 -0300

    avances me tirar nan

--- parte2.1/Runge_Kutta.m
+++ parte2.1/Runge_Kutta.m
@@ -1,10 +1,10 @@
-function Runge_Kutta (dt, x0, v0)
+function Runge_Kutta (intervals, tmax, dt, x0, v0)
     phi = cell(3,4);
     tao1 = 1/2;
     tao2 = 1;
 
 
-    a = @(u) (-(100)*(1+10*u*u)*u);
+    a = @(u) (-((100*u) + (1000*(u^3))));
     
 
     phi{1,1} = @(t) (1);
@@ -15,14 +15,18 @@ function Runge_Kutta (dt, x0, v0)
     phi{2,3} = @(t) ((t*t) / (tao1*dt));
 
     phi{3,1} = @(t) (1);
-    phi{3,2} = @(t) ((t*(2*t*t - 3*tao1*dt*t -3*tao2*dt*t + 6*tao1*tao2*dt*dt)) / (6*tao1*tao2*dt*dt));
-    phi{3,3} = @(t) (-(t*t*(-2*t + 3*tao2*dt) / (6*tao1*(-tao2+tao1)*dt*dt)));
-    phi{3,4} = @(t) ((t*t*(-2*t + 3*tao1*dt) / (6*tao2*(-tao2+tao1)*dt*dt)));
-
-    disp(phi{1,1}(0))
+    phi{3,2} = @(t) ((t*(2*(t^2) - 3*tao1*dt*t -3*tao2*dt*t + 6*tao1*tao2*(dt^2))) / (6*tao1*tao2*(dt^2)));
+    phi{3,3} = @(t) (-((t^2) - 2*t + 3*tao2*dt) / (6*tao1*(-tao2+tao1)*(dt^2)));
+    phi{3,4} = @(t) ( ((t^2) - 2*t + 3*tao1*dt) / (6*tao2*(-tao2+tao1)*(dt^2)));
 
     tao = {1/2, 1, 1};
-    resultados = zeros(3,2);
+    
+    time = 0:dt:tmax;
+    x = zeros(1, intervals);
+    x(1) = x0;
+    v = zeros(1, intervals);
+    v(1) = v0;
+    resRK = zeros(2,2);
 
     alpha = cell(3,3);
     alpha{1,1} = 0;
@@ -31,34 +35,36 @@ function Runge_Kutta (dt, x0, v0)
     alpha{3,1} = 0;
     alpha{3,2} = 0;
     alpha{3,3} = 0;
+  
+    ts = 1;
+    while (ts < intervals)
+        %Primer orden
+        %x_{t_s + tao1*dt}
+        resRK(1,1) = phi{1, 1}(tao{1}*dt)*x(ts) + phi{1,2}(tao{1}*dt)*v(ts) + ((0.5)*(dt^2)*(alpha{1,1}*a(x(ts))));
+        %v_{t_s + tao1*dt}
+        resRK(1,2) = phi{1, 1}(tao{1}*dt)*v(ts) + phi{1,2}(tao{1}*dt)*a(x(ts));
 
-    disp(phi{1, 1}(tao{1}*dt));
+        %Segundo orden
+        %x_{t_s + tao2*dt}
+        resRK(2,1) = phi{2,1}(dt*tao{2})*x(ts) + phi{2,2}(dt*tao{2})*v(ts) + phi{2,3}(dt*tao{2})*resRK(1,2) + ( 0.5*(dt^2)*( (alpha{2,1}*a(x(ts))) + (alpha{2,2}*a(resRK(1,1))) ) );
+        %v_{t_s + tao2*dt}
+        resRK(2,2) = phi{2,1}(tao{2}*dt)*v(ts) + phi{2,2}(tao{2}*dt)*a(x(ts)) + phi{2,3}(tao{2}*dt)*a(resRK(1,1));
+        
+        %Tercer orden
+        %x_{t_s + dt}
+        x(ts + 1) = phi{3,1}(dt)*x(ts) + phi{3,2}(dt)*v(ts) + phi{3,3}(dt)*resRK(1,2) + phi{3,4}(dt)*resRK(2,2) + ( 0.5*(dt^2)*( (alpha{3,1}*a(x(ts))) + (alpha{3,2}*a(resRK(1,1))) +(alpha{3,3}*a(resRK(2,1))) ) );
+        %v_{t_s + dt}
+        v(ts + 1) = phi{3,1}(dt)*x(ts) + phi{3,2}(dt)*v(ts) + phi{3,3}(dt)*resRK(1,2) + phi{3,4}(dt)*a(resRK(1,2));
 
-    for i=1:3
-        resultados(i,1) = phi{i, 1}(tao{i}*dt)*x0 + phi{i,2}(tao{i}*dt)*v0;
-        resultados(i,2) = phi{i, 1}(tao{i}*dt)*v0 + phi{i,2}(tao{i}*dt)*a(x0);
-
-        if (i == 2)
-            resultados(i,1) = resultados(i, 1) + phi{i, 3}(dt*tao{i}) * resultados(1,2) + ((tao{i}*dt * tao{i}*dt) / 2) * (alpha{i,1}*a(x0) + alpha{i,i}*a(resultados(i-1,1)));
-
-            resultados(i,2) = resultados(i,2) + phi{i,3}(tao{i}*dt) * a(resultados(i-1, 1));
-        endif
-        if (i == 3)
-            resultados(i,1) = resultados(i, 1) + phi{i, 3}(dt*tao{i}) * resultados(1,2)+ phi{i, 4}(dt*tao{i}) * resultados(2,2) + ((tao{i}*dt * tao{i}*dt) / 2) * (alpha{i,i-2}*a(x0) + alpha{i,i-1}*a(resultados(i-2,1)) + alpha{i,i}*a(resultados(i-1,1)));
-
-            resultados(i,2) = resultados(i,2) + phi{i,3}(tao{i}*dt) * a(resultados(i-2, 1)) + phi{i,4}(tao{i}*dt) * a(resultados(i-1, 1));
-        endif
-    endfor
-    disp(resultados);
-
-    ploting = cell(2,30);
-    for i=1:30
-        ploting{1,i} = i;
-        ploting{2,i} = i*5;
-    endfor
-
-    plot(ploting{1, : }, ploting{2, :});
+        ts = ts + 1;
+    endwhile
     
+    display(time);
+    display(x);
+    plot(time, x,"r");
+    xlabel ("ts");
+    ylabel ("u(ts)");
+    title ("Simple 2-D Plot");
 end
 
 
