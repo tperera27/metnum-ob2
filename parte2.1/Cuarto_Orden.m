fb79495da64738de32682950b71bb71d89e24e55
Tatiana Perera <91163751+tperera27@users.noreply.github.com>
2021-11-13 17:23:32 -0300

    todos listo (#5)

--- parte2.1/Cuarto_Orden.m
+++ parte2.1/Cuarto_Orden.m
@@ -1,82 +1,86 @@
 function Cuarto_Orden (intervals, tmax, dt, x0, v0)
-    phi = cell(4,5);
-    tao1 = 1/2;
-    tao2 = 1;
-    tao3 = 1;
-
-
+    
+    %Calculo aceleración
     a = @(u) (-((100*u) + (1000*(u**3))));
     
+    %Phi precalculado
+    phiCO = cell(4,5);
 
-    phi{1,1} = 1;
-    phi{1,2} = dt / 3;
+    phiCO{1,1} = 1;
+    phiCO{1,2} = dt / 3;
 
-    phi{2,1} = 1;
-    phi{2,2} = dt/8;
-    phi{2,3} = (3/8)*dt;
+    phiCO{2,1} = 1;
+    phiCO{2,2} = dt/8;
+    phiCO{2,3} = (3/8)*dt;
 
-    phi{3,1} = 1;
-    phi{3,2} = (dt/2);
-    phi{3,3} = -(3/2)*dt;
-    phi{3,4} = 2*dt;
+    phiCO{3,1} = 1;
+    phiCO{3,2} = (dt/2);
+    phiCO{3,3} = -(3/2)*dt;
+    phiCO{3,4} = 2*dt;
     
-    phi{4,1} = 1;
-    phi{4,2} = (dt/6);
-    phi{4,3} = 0;
-    phi{4,4} = (2/3)*dt;
-    phi{4,5} = (dt/6);
+    phiCO{4,1} = 1;
+    phiCO{4,2} = (dt/6);
+    phiCO{4,3} = 0;
+    phiCO{4,4} = (2/3)*dt;
+    phiCO{4,5} = (dt/6);
 
-    tao = {1/3, 1/2, 1};
+    %Parámetros    
+    taoCO = {1/3, 1/2, 1};
+    alphaCO = cell(4,4);
+    alphaCO{1,1} = 1;
+    alphaCO{2,1} = -3/5;
+    alphaCO{2,2} = 3/5;
+    alphaCO{3,1} = 3/5;
+    alphaCO{3,2} = 3/5;
+    alphaCO{3,3} = 0;
+    alphaCO{4,1} = 0;
+    alphaCO{4,2} = 0;
+    alphaCO{4,3} = 0;
+    alphaCO{4,4} = 0;
     
+    %Vecor con los tiempos a graficar     
     time = 0:dt:tmax;
-    x = zeros(1, intervals);
-    x(1) = x0;
-    v = zeros(1, intervals);
-    v(1) = v0;
-    res4 = zeros(3,2);
-
-    alpha = cell(4,4);
-    alpha{1,1} = 1;
-    alpha{2,1} = -3/5;
-    alpha{2,2} = 3/5;
-    alpha{3,1} = 3/5;
-    alpha{3,2} = 3/5;
-    alpha{3,3} = 0;
-    alpha{4,1} = 0;
-    alpha{4,2} = 0;
-    alpha{4,3} = 0;
-    alpha{4,4} = 0;
-        
+    
+    %Posición
+    xCO = zeros(1, intervals);
+    xCO(1) = x0;
+    
+    %Velocidad
+    vCO = zeros(1, intervals);
+    vCO(1) = v0;
+    
+    resCO = zeros(3,2);
+ 
     ts = 1;
     while (ts < intervals)
         %Ecuacion 4 y 5
         %x_{t_s + dt/3}
-        res4(1,1) = x(ts) + (dt/3)*v(ts) + (1/18)*a(x(ts));
+        resCO(1,1) = xCO(ts) + (dt/3)*vCO(ts) + (1/18)*a(xCO(ts));
         %v_{t_s + dt/3}
-        res4(1,2) = v(ts) + (dt/3)*a(x(ts));
+        resCO(1,2) = vCO(ts) + (dt/3)*a(xCO(ts));
 
         %Ecuacion 8 y 9
         %x_{t_s + dt/2}
-        res4(2,1) = x(ts) + (dt/2)*v(ts) + ((dt^2)/8)*( ((2/5)*a(x(ts))) + ((3/5)*a(res4(1,1))) );
+        resCO(2,1) = xCO(ts) + (dt/2)*vCO(ts) + ((dt^2)/8)*( ((2/5)*a(xCO(ts))) + ((3/5)*a(resCO(1,1))) );
         %v_{t_s + dt/2}
-        res4(2,2) = v(ts) + (dt/8)*a(x(ts)) + ((3*dt)/8)*a(res4(2,1));
+        resCO(2,2) = vCO(ts) + (dt/8)*a(xCO(ts)) + ((3*dt)/8)*a(resCO(2,1));
         
         %Ecuacion 28 y 29
         %x_{t_s + dt}
-        res4(3,1) = x(ts) + dt*v(ts) + (1/2)*(dt^2)*( ((1/10)*a(x(ts))) - ((3/5)*a(res4(1,1))) + ((3/2)*a(res4(2,1))) );
+        resCO(3,1) = xCO(ts) + dt*vCO(ts) + (1/2)*(dt^2)*( ((1/10)*a(xCO(ts))) - ((3/5)*a(resCO(1,1))) + ((3/2)*a(resCO(2,1))) );
         %v_{t_s + dt}
-        res4(3,2) = v(ts) + (1/2)*dt*a(x(ts)) + 2*dt*a(res4(2,1));
+        resCO(3,2) = vCO(ts) + (1/2)*dt*a(xCO(ts)) + 2*dt*a(resCO(2,1));
         
         %Ecuacion 30 y 31
         %x_{t_s + dt}
-        x(ts + 1) = x(ts) + dt*v(ts) + (1/2)*(dt^2)*( (2*a(x(ts))) - (3*a(res4(1,1))) + (7*a(res4(2,1))) );
+        xCO(ts + 1) = xCO(ts) + dt*vCO(ts) + (1/2)*(dt^2)*( (2*a(xCO(ts))) - (3*a(resCO(1,1))) + (7*a(resCO(2,1))) );
         %v_{t_s + dt}
-        v(ts + 1) = v(ts) + (1/6)*dt*a(x(ts)) + (2/3)*dt*a(res4(2,1)) + (1/6)*dt*a(res4(3,1));
+        vCO(ts + 1) = vCO(ts) + (1/6)*dt*a(xCO(ts)) + (2/3)*dt*a(resCO(2,1)) + (1/6)*dt*a(resCO(3,1));
         
         ts = ts + 1;
     endwhile
     
-    plot(time, x,"r");
+    plot(time, xCO,"r");
     xlabel ("t");
     ylabel ("u(t)");
     title ("");
---
+++ parte2.1/Tercer_Orden.m
@@ -0,0 +1,74 @@
+function Tercer_Orden (intervals, tmax, dt, x0, v0)
+    
+    %Calculo aceleración
+    a = @(u) (-((100*u) + (1000*(u**3))));
+    
+    %Phi precalculado
+    phiTO = cell(3,4);
+    
+    phiTO{1,1} = 1;
+    phiTO{1,2} = dt/3;
+
+    phiTO{2,1} = 1;
+    phiTO{2,2} = 0;
+    phiTO{2,3} = (2*dt) / 3;
+
+    phiTO{3,1} = 1;
+    phiTO{3,2} = (dt/4);
+    phiTO{3,3} = 0;
+    phiTO{3,4} = ((3*dt)/4);
+
+    %Parámetros    
+    taoTO = {1/3, 2/3, 1};
+    alphaTO = cell(3,3);
+    alphaTO{1,1} = 1;
+    alphaTO{2,1} = -2/3;
+    alphaTO{2,2} = 2/3;
+    alphaTO{3,1} = 1/3;
+    alphaTO{3,2} = -2/3;
+    alphaTO{3,3} = 1/3;
+    
+    %Vector con los tiempos a graficar    
+    time = 0:dt:tmax;
+    
+    %Posición    
+    xTO = zeros(1, intervals);
+    xTO(1) = x0;
+    
+    %Velocidad
+    vTO = zeros(1, intervals);
+    vTO(1) = v0;
+    
+    %Resultados intermedios
+    resTO = zeros(2,2);
+  
+    ts = 1;
+    while (ts < intervals)
+        %Primer orden
+        %x_{t_s + tao1*dt}
+        resTO(1,1) = phiTO{1, 1}*xTO(ts) + phiTO{1,2}*vTO(ts) + ((0.5)*((taoTO{1}*dt)**2)*(alphaTO{1,1}*a(xTO(ts))));
+        %v_{t_s + tao1*dt}
+        resTO(1,2) = phiTO{1, 1}*vTO(ts) + phiTO{1,2}*a(xTO(ts));
+
+        %Segundo orden
+        %x_{t_s + tao2*dt}
+        resTO(2,1) = phiTO{2,1}*xTO(ts) + phiTO{2,2}*vTO(ts) + phiTO{2,3}*resTO(1,2) + ( 0.5*((taoTO{2}*dt)**2)*( (alphaTO{2,1}*a(xTO(ts))) + (alphaTO{2,2}*a(resTO(1,1))) ) );
+        %v_{t_s + tao2*dt}
+        resTO(2,2) = phiTO{2,1}*vTO(ts) + phiTO{2,2}*a(xTO(ts)) + phiTO{2,3}*a(resTO(1,1));
+        
+        %Tercer orden
+        %x_{t_s + dt}
+        xTO(ts + 1) = phiTO{3,1}*xTO(ts) + phiTO{3,2}*vTO(ts) + phiTO{3,3}*resTO(1,2) + phiTO{3,4}*resTO(2,2) + ( 0.5*(dt**2)*( (alphaTO{3,1}*a(xTO(ts))) + (alphaTO{3,2}*a(resTO(1,1))) +(alphaTO{3,3}*a(resTO(2,1))) ) );
+        %v_{t_s + dt}
+        vTO(ts + 1) = phiTO{3,1}*vTO(ts) + phiTO{3,2}*a(xTO(ts)) + phiTO{3,3}*a(resTO(1,1)) + phiTO{3,4}*a(resTO(2,1));
+        
+        ts = ts + 1;
+    endwhile
+    
+    plot(time, x, "b");
+    xlabel ("Tiempo", "fontsize", 20);
+    ylabel ("Posición", "fontsize", 20);
+    title ("Resorte elástico - Método de Runge-Kutta de tercer orden", "fontsize", 30);
+end
+
+
